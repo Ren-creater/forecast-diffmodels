36cba6624e68d93821ded639f7bec682b110697b
Zhibo Ren <zhiboren630@gmail.com>
2024-07-17 15:21:37 +0100

    modifed directories

--- imagen/64_FC/v_64_FC.py
+++ imagen/64_FC/v_64_FC.py
@@ -19,7 +19,7 @@ from torch.utils.tensorboard import SummaryWriter
 from einops import rearrange
 
 RUN_NAME = "64_FC"
-BASE_DIR = f"/vol/bitbucket/pn222/models/{RUN_NAME}"
+BASE_DIR = f"/vol/bitbucket/zr523/models/{RUN_NAME}"
 
 os.makedirs(BASE_DIR, exist_ok=True)
 logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", 
--- imagen/64_FC/v_gpu_cluster.sh
+++ imagen/64_FC/v_gpu_cluster.sh
@@ -3,4 +3,4 @@
 #SBATCH --partition=gpgpuB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=zr523@ic.ac.uk
-/vol/bitbucket/pn222/venv/bin/python /homes/pn222/Work/MSc_Project_pn222/imagen/64_FC/v_64_FC.py -mode execute -epochs 261
+/vol/bitbucket/zr523/myvenv/bin/python /homes/zr523/forecast-diffmodels/imagen/64_FC/v_64_FC.py -mode execute -epochs 261
