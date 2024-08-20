6793b10a42a9cd090e4ef8aad2c08233c9f4ad20
Zhibo Ren <zhiboren630@gmail.com>
2024-08-20 12:49:17 +0100

    v_hpc_cluster.sh

--- imagen/64_FC/v_hpc_cluster.sh
+++ imagen/64_FC/v_hpc_cluster.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-#PBS -l walltime=96:00:00
 #PBS -l select=1:ncpus=4:mem=24gb:ngpus=1:gpu_type=RTX6000
+#PBS -l walltime=72:00:00
 
 module load anaconda3/personal
 
 source activate research_env
 
-python3 -m pip install $HOME/researchProject/forecast-diffmodels/imagen/requirements.txt
+#conda install python=3.10.12
 
-python $HOME/researchProject/forecast-diffmodels/imagen/64_FC/v_64_FC.py -mode execute -epochs 261
+python3 -m pip install -r $HOME/researchProject/forecast-diffmodels/imagen/requirements.txt
+
+#python $HOME/researchProject/forecast-diffmodels/imagen/64_FC/v_64_FC.py -mode execute -epochs 261
+
+cd $HOME/researchProject/forecast-diffmodels/imagen/64_FC
+
+python v_64_FC.py -mode execute -epochs 261
