bb947808160918484838a089e36c186d7221d4e8
Zhibo Ren <zhiboren630@gmail.com>
2024-09-15 11:12:55 +0100

    try again

--- imagen/64_FC/v_test_v_FC_dim128.py
+++ imagen/64_FC/v_test_v_FC_dim128.py
@@ -38,7 +38,7 @@ best_epoch_dict = {
     "v_64_FC_3e-4": 390,
     "v_64_FC_3e-4_dim64": 390,
     "v_64_FC_3e-4_dim64_img": 390,
-    "v_64_FC_3e-4_dim128": 344
+    "v_64_FC_3e-4_dim128": 340
 }
 
 unet1 = Unet3D(
