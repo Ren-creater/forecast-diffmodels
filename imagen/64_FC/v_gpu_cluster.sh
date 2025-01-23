0ec9e72a59194d77c9947d9d6feb7e606520e859
Zhibo Ren <zr523@login-ai.cx3.hpc.ic.ac.uk>
2025-01-23 20:21:14 +0000

    deleted uncessary files

--- imagen/64_FC/v_gpu_cluster.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --gres=gpu:1
-#SBATCH --partition=gpgpuB
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=zr523@ic.ac.uk
-/rds/general/user/zr523/home/myvenv/bin/python /rds/general/user/zr523/home/researchProject/forecast-diffmodels/imagen/64_FC/v_64_FC.py -mode execute -epochs 261
